1d80db6eb6fa79f0c09383d9da9605a4752714ae
Christopher Kearl <c2_kearl@yahoo.com>
2024-09-21 22:36:38 -0600

    favicon, title, index, and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.html',
+    './src/**/*.react',
+    './src/**/*.jsx',
+    './src/**/*.js',
+    './src/**/*.ts',
+    './src/**/*.tsx',
+  ],
   theme: {
     extend: {},
   },
